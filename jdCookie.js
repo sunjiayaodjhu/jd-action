ef7221c3b29e582fe1e35362f8f395c280328cce
sunjiayaodjhu <504738323@qq.com>
2021-04-13 14:04:50 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck
+  'pin=sunjiayao111;wskey=AAJgc7nLAEDdx3w3EE3mHgKHNzWU6fu-ySvub2Ow1dcSbA0JIjLer3MpYJPTn7wCZJWLLp1XB15v0vLgeA55_5Ckw4DOc1Gj;whwswswws=b22Qwlu+va+jRs3AcPJstEKws5af3XPee7ApkYS4JtG5jZ2zUt2g3EQDjm5zsVkU/KrJ3SK94wXx9WDn2O36IQA==;unionwsws={"jmafinger":"b22Qwlu+va+jRs3AcPJstEKws5af3XPee7ApkYS4JtG5jZ2zUt2g3EQDjm5zsVkU\/KrJ3SK94wXx9WDn2O36IQA==","devicefinger":"eidI9c1381218dsc+DsA9eZSSC2MgH3UEPChm\/g7Amx0mcX\/u0\/\/+jRmpR0kjaU7Gsc880QyvUKxfxuzs2YIbI7COtcUJ14W7mxTFwZAMIHiysMHHUVx"}',//账号一ck
   '',//账号二ck,如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
